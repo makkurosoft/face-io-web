93f9dc4e2e2c8362afc40174c9ce4cc9926ef6eb
kbc16a09 <kbc16a09@stu.kawahara.ac.jp>
2020-02-05 00:02:43 +0900

    ダッシュボードページにcontainer適用

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,12 +1,13 @@
+<div class="container">
+	<a href="<?php echo base_url() . "main/signout"?>">サインアウト</a>
+	<h1><?php echo $user_name;?>さんの管理クラス一覧</h1>
 
-<a href="<?php echo base_url() . "main/signout"?>">サインアウト</a>
-<h1><?php echo $user_name;?>さんの管理クラス一覧</h1>
-
-<!-- クラス一覧をリンクで表示 -->
-<?php
-foreach($classes as $class){
+	<!-- クラス一覧をリンクで表示 -->
+	<?php
+	foreach($classes as $class){
 	?>
 	<a href="<?php echo base_url() . "main/dashboard/" . $class['class_id']?>"><?php echo $class['department_name'] . $class['grade'] ?></a><br>
 	<?php
-}
-?>
+	}
+	?>
+</div>
