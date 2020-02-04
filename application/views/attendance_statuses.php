6a66cc8de90f7b0fb0529f0016df9d1a052fd28a
kbc16a09 <kbc16a09@stu.kawahara.ac.jp>
2020-02-04 21:05:18 +0900

    出席一覧ページをtableに変更

--- application/views/attendance_statuses.php
+++ application/views/attendance_statuses.php
@@ -3,10 +3,19 @@
 <a href="<?php echo base_url() . "main/dashboard"?>">ダッシュボード</a>
 <h1><?php echo $class_id;?>の出席状況</h1>
 
-<?php
-foreach($attendance_statuses as $attendence_status){
-	?>
-	<h3><?php echo $attendence_status['name'] . $attendence_status['出欠判定'] ?></h3>
+<table>
+	<tr>
+		<th>氏名</th><th>出欠</th>
+	</tr>
 	<?php
-}
-?>
+	foreach($attendance_statuses as $attendence_status){
+	?>
+		<tr>
+		<td><?php echo $attendence_status['name'] ?> </td>
+		<td><?php echo $attendence_status['出欠判定'] ?></td>
+		</tr>
+	<?php
+	}
+	?>
+
+</table>
