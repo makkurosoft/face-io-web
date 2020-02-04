018bd2282138f1a7da206be11e80d2a25c519509
kbc16a09 <kbc16a09@stu.kawahara.ac.jp>
2020-02-04 20:37:28 +0900

    サインイン画面にbootstrap適用

--- application/views/signin.php
+++ application/views/signin.php
@@ -1,17 +1,20 @@
 
 <h1>Sign in</h1>
-
 <?php echo form_open("main/signin_validation"); ?>
 
-<h5>ユーザ名</h5>
-<input type="text" name="user_name" value="<?php echo set_value('user_name'); ?>" size="30" />
-<?php echo form_error('user_name'); ?>
-
-<h5>パスワード</h5>
-<input type="password" name="password" value=""; size="30" />
-<?php echo form_error('password'); ?>
-
-<div>
-    <input type="submit" value="Sign in" />
+<div class="form-group row">
+    <label for="user_name" class="col-sm-2 col-form-label">ユーザ名</label>
+    <div class="col-sm-8">
+        <input type="text" class="form-control" name="user_name">
+    </div>
+    <?php echo form_error('user_name'); ?>
 </div>
+<div class="form-group row">
+    <label for="password" class="col-sm-2 col-form-label">パスワード</label>
+    <div class="col-sm-8">
+        <input type="password" class="form-control" name="password">
+    </div>
+    <?php echo form_error('password'); ?>
+</div>
+<button type="submit" class="btn btn-primary">Sign in</button>
 </form>
