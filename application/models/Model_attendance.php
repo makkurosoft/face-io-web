69b2d7a9e51d20de1b4b51986336f4f9e948b413
3getrobo <kbc16a16@stu.kawahara.ac.jp>
2020-02-05 11:41:07 +0900

    正しい生徒一覧が表示されるようSQL文を修正

--- application/models/Model_attendance.php
+++ application/models/Model_attendance.php
@@ -23,12 +23,7 @@ class Model_attendance extends CI_Model{
       RIGHT JOIN Label ON Attendance.label_id = Label.label_id
       RIGHT JOIN Belongs ON Label.student_id = Belongs.student_id
       LEFT JOIN Student ON Belongs.student_id = Student.student_id
-      WHERE (date BETWEEN
-        UNIX_TIMESTAMP(CURDATE() + INTERVAL 6 HOUR)
-        AND
-        UNIX_TIMESTAMP(CURDATE() + INTERVAL 23 HOUR + INTERVAL 59 MINUTE + INTERVAL 59 SECOND)
-        OR date IS NULL)
-        AND Belongs.class_id = ?
+      WHERE Belongs.class_id = ?
         GROUP BY Belongs.attendance_number, Student.name";
         $query = $this->db->query($sql, array($class_id));
         return $query->result_array();
