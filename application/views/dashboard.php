a1be184bc5fa9f22dab532851dedaed6175ac426
kbc16a09 <kbc16a09@stu.kawahara.ac.jp>
2020-02-08 21:56:53 +0900

    ダッシュボードページにナビゲーションバーを追加

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,6 +1,25 @@
 <div class="container">
-	<a href="<?php echo base_url() . "main/signout"?>">サインアウト</a>
-	<h1><?php echo $user_name;?>さんの管理クラス一覧</h1>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<a class="navbar-brand" href="<?php echo base_url() . "main/dashboard"?>">Face-io</a>
+		<div class="collapse navbar-collapse justify-content-start">
+			<ul class="navbar-nav">
+				<li class="nav-item active">
+					<a class="nav-link" href="<?php echo base_url() . "main/dashboard"?>">ダッシュボード</a>
+				</li>
+			</ul>
+		</div>
+		<li class="nav-item dropdown list-unstyled">
+			<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				<?php echo $user_name?>
+			</a>
+			<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+				<a class="dropdown-item" href="<?php echo base_url() . "main/dashboard"?>">ダッシュボード</a>
+				<a class="dropdown-item" href="<?php echo base_url() . "main/signout"?>">サインアウト</a>
+			</div>
+		</li>
+	</nav>
+
+	<h1>管理クラス一覧</h1>
 
 	<!-- クラス一覧をリンクで表示 -->
 	<?php
